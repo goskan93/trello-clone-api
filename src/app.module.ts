0836c129302cd56d17ecdb218965974f8a9d7774
goskan93 <natalia.goska@gmail.com>
2023-03-17 16:20:51 +0100

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,12 @@ import { CardModule } from './modules/cards/card.module';
 import { TaskModule } from './modules/tasks/task.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(''), CardModule, TaskModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://localhost:testestestest@local-env.dwokr44.mongodb.net/trello-clone-api?retryWrites=true&w=majority',
+    ),
+    CardModule,
+    TaskModule,
+  ],
 })
 export class AppModule {}
