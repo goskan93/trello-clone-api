bee9e3bc273726ee2634c8b4d6b080e5fbea58c7
goskan93 <natalia.goska@gmail.com>
2023-06-11 18:08:45 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   await app.listen(3004);
