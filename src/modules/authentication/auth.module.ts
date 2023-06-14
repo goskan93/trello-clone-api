a5b8f70615223addc4d33d978a7290c51487690f
goskan93 <natalia.goska@gmail.com>
2023-06-14 13:29:28 +0200

    .

--- src/modules/authentication/auth.module.ts
+++ src/modules/authentication/auth.module.ts
@@ -8,7 +8,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [`${process.env.NODE_ENV}.env`],
+      envFilePath: [`.env`],
       isGlobal: true,
       cache: true,
     }),
