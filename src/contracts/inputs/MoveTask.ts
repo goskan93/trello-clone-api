9fcbbf6c8e6124a464fc8c32ff721c5a37d2adb4
goskan93 <natalia.goska@gmail.com>
2023-06-22 19:10:21 +0200

    remove contracts

--- src/contracts/inputs/MoveTask.ts
+++
@@ -1,6 +0,0 @@
-export default interface MoveTask {
-  fromCardId: string;
-  toCardId: string;
-  taskId: string;
-  index: number;
-}
--- src/contracts/outputs/TaskOutput.ts
+++
@@ -1,6 +0,0 @@
-export default interface TaskOutput {
-  id: string;
-  name: string;
-  cardId: string;
-  index: number;
-}
